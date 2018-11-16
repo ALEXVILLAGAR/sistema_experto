102ec52494bac68057faef71e1c4214cc1277e8d
unknown <Jhones0507>
2018-11-16 00:33:59 -0500

    Ajustes vista diagnóstico

--- resources/views/padecimientos.blade.php
+++ resources/views/padecimientos.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+  
+  <div id="contresult">
+    <h2 class="tdiagnost">DIAGNÓSTICO GENERADO</h2>
+    <p>Las siguientes enfermedades respiratorias se presentan con un porcentaje aproximado de posibilidad de que usted padezca de dicha enfermedad, adicionalmente le ofrecemos una descripción y posible tratamiento para estos padecimientos, por favor léalo con atención y acuda oportunamente a revisión médica en su centro de salud más cercano.</p><hr>
     @foreach($enfermedades as $key=>$value)
-      <h3>{{$key}}</h3>
-      <strong> {{$value}} </strong>
-
+      <h4><strong>{{$value*10}} % = </strong> {{$key}}</h4>
     @endforeach
+  </div>
+
 @endsection()
--- resources/views/pronostico.blade.php
+++ resources/views/pronostico.blade.php
@@ -2,24 +2,41 @@
 @extends('header')
 
 @section('opcion')
-<li class="nav-item"> <i ></i><a href="{{ url('nosotros') }}" class="text-white">pronóstico</a>
+  <li class="nav-item"> <i ></i><a href="{{ url('nosotros') }}" class="text-white">pronóstico</a>
 @endsection
 
 @section('content')
-<div class="container" id="contpronost">
+
+  <div class="container" id="contpronost">
+    
     <div>
-        <h1 class="text-info text-center"> <strong>Tu pronóstico a tiempo</strong> <hr class="bg bg-danger"></h1>
+      <h2 class="text-center"><strong>Tu pronóstico a tiempo</strong></h2>
+      <p>Por favor selecciona los síntomas que presentas para que podamos brindarte <br>un diagnóstico adecuado sobre la enfermedad que puedes pesentar</p>
+      <hr>
     </div>
-    <div>
-        <form action="{{route('enfermedades.store')}}" method="post">
-            @csrf
-            @foreach($sintomas as $sintoma)
-                <input type="checkbox" name="{{$sintoma->id}}">
-                <label> {{$sintoma->sintoma}} </label>
-                <br>
-            @endforeach
-            <button type="submit" class="btn btn-success">Diagnosticar</button>
-        </form>
-    </div>
-</div>
+    
+    <form action="{{route('enfermedades.store')}}" method="post">
+      @csrf
+      
+      <div class="form-check">
+        @foreach($sintomas as $sintoma)
+          <label>
+            <input type="checkbox" name="{{$sintoma->id}}"> 
+            <span class="label-text">{{$sintoma->sintoma}}</span>
+          </label><br>
+        @endforeach
+      </div>
+      
+      <span class="imgdiagnost"></span>
+      <span class="imgdiagnost"></span>
+      <span class="imgdiagnost"></span>
+      <span class="imgdiagnost"></span>
+
+      <button type="submit" class="btn btn-primary">Diagnosticar</button>
+    
+    </form>
+  
+
+  </div>
+
 @endsection
