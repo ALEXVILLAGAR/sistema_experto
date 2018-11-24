c342b24aecc74dd1a9ada74377fb656d4361d097
unknown <Jhones0507>
2018-11-24 18:21:21 -0500

    merge2411

--- resources/views/algoritmo.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>
-    aun no hay algoritmo
-</h1>
-@endsection
---
+++ resources/views/modal_condiciones.blade.php
@@ -0,0 +1,51 @@
+<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade" id="exampleModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content border border-primary">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="exampleModalLabel">
+                    <strong>
+                        Terminos y condiciones
+                    </strong>
+                </h5>
+                <button aria-label="Close" class="close" data-dismiss="modal" type="button">
+                    <span aria-hidden="true">
+                        ×
+                    </span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h4>
+                    Te proporcionamos una experiencia personalizada:
+                </h4>
+                <br>
+                    tus vivencias en Facebook son distintas de las experiencias del resto de personas que usan la plataforma. Todos los aspectos están personalizados: desde las publicaciones, las historias, los anuncios y otros contenidos que ves en la sección de noticias o en nuestra plataforma de vídeo, hasta las páginas que sigues y otras funciones que quizás usas, como Tendencias, Marketplace y la búsqueda. Usamos los datos que tenemos, por ejemplo, los relativos a las conexiones que entablas, las opciones y los ajustes de configuración que seleccionas, el tipo de contenido que compartes y las acciones que llevas a cabo en nuestros Productos o fuera de ellos para personalizar tu experiencia
+                </br>
+                <hr>
+                    <h4>
+                        <br>
+                            Te ayudamos a descubrir una enfermedad que padece segun los sintomas proporcionados al sistema.
+                        </br>
+                    </h4>
+                    <br>
+                        Te mostramos anuncios, ofertas y otro contenido patrocinado para que descubras el contenido, los productos y los servicios que ofrecen los numerosos negocios y organizaciones que usan Facebook y los Productos de Facebook. Nuestros socios nos pagan para que te mostremos su contenido, y nosotros diseñamos nuestros servicios para que el contenido patrocinado que ves te resulte tan útil y relevante como todo lo que se muestra en nuestros Productos.
+                    </br>
+                    <hr>
+                        <h4>
+                            <br/>
+                            privacidad de informacion.
+                        </h4>
+                        Para proporcionarte estos servicios, debemos recopilar y usar tus datos personales. En la Política de datos, que debes aceptar para poder usar nuestros Productos, incluimos información detallada sobre nuestras prácticas.
+                        <br>
+                            Estos datos son de uso solo para el sistema y el sistema no tiene permiso para hacer uso diferente de estos, ademas de utilizarlos para los pronosticos de cada usuario.
+                        </br>
+                    </hr>
+                </hr>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" data-dismiss="modal" type="button">
+                    cerrar
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
